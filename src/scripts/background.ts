d44f1533fc0aa7a1fc912376f175b79123d430c0
iantanwx <ian.tan@voyage.finance>
2022-04-13 17:23:42 +0800

    refactor: move mm connection to background

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,10 +1,40 @@
-import { Runtime } from 'webextension-polyfill-ts';
 import PortStream from 'extension-port-stream';
+import { detect } from 'detect-browser';
+import browser, { Runtime } from 'webextension-polyfill';
+import { Duplex } from 'stream';
+import pump from 'pump';
+
+// taken from https://github.com/MetaMask/providers/blob/0c0eb500c76e7c03c7e9c9133ec2eaa311000cf0/src/extension-provider/external-extension-config.json
+const MM_EXTENSION_ID = {
+  CHROME: 'nkbihfbeogaeaoehlefnkodbefgpgknn',
+  FIREFOX: 'webextension@metamask.io',
+};
 
 chrome.runtime.onConnect.addListener((port) => {
   console.log('got incoming connection from: ', port);
-  const stream = new PortStream(port as Runtime.Port);
+  const stream = new PortStream(port as Runtime.Port) as unknown as Duplex;
+  if (port.name === 'voyage-popup') {
+    setupPopupProvider(stream);
+  }
   stream.on('data', (data) => {
     console.log(`background got data from ${port.name}: `, data);
   });
 });
+
+function setupPopupProvider(popupStream: Duplex) {
+  const currentMetaMaskId = getMetaMaskExtensionId();
+  const metaMaskPort = browser.runtime.connect(currentMetaMaskId);
+  const metaMaskStream = new PortStream(metaMaskPort);
+  pump(popupStream, metaMaskStream, popupStream);
+}
+
+function getMetaMaskExtensionId() {
+  const browserInfo = detect();
+  switch (browserInfo?.name) {
+    case 'firefox':
+      return MM_EXTENSION_ID.FIREFOX;
+    case 'chrome':
+    default:
+      return MM_EXTENSION_ID.CHROME;
+  }
+}
--- src/ui/pages/Popup/index.tsx
+++ src/ui/pages/Popup/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createExternalExtensionProvider } from '@metamask/providers';
 import './index.css';
 import Popup from './Popup';
+import PortStream from 'extension-port-stream';
+import browser from 'webextension-polyfill';
+import { BaseProvider } from '@metamask/providers';
+import { Duplex } from 'stream';
 
 async function bootstrap() {
-  const provider = createExternalExtensionProvider();
+  // set up a connection to background script
+  const port = browser.runtime.connect({ name: 'voyage-popup' });
+  const providerStream = new PortStream(port) as unknown as Duplex;
+  const provider = new BaseProvider(providerStream);
   ReactDOM.render(
     <React.StrictMode>
       <Popup provider={provider} />
