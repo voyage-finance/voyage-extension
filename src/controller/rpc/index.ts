168f240c8a32582f9464c89a9a91a8a59eb87adc
iantanwx <ian.tan@voyage.finance>
2022-07-26 18:09:44 +0800

    feat: stub VoyageRpcService

--- src/controller/rpc/index.ts
+++ src/controller/rpc/index.ts
@@ -1,16 +1,2 @@
-import { ethers } from 'ethers';
-import { createAsyncMiddleware } from 'json-rpc-engine';
-
-export const createProviderMiddleware = (
-  provider: ethers.providers.JsonRpcProvider
-) =>
-  createAsyncMiddleware(async (req, res, next) => {
-    try {
-      res.result = await provider.send(req.method, req.params as unknown[]);
-    } catch (error: any) {
-      if (error.body) {
-        throw JSON.parse(error.body as string);
-      }
-      throw error;
-    }
-  });
+export { createVoyageMiddleware, createProviderMiddleware } from './middleware';
+export * from './service';
---
+++ src/controller/rpc/service.ts
@@ -0,0 +1,19 @@
+import { ControllerStore } from '../store';
+
+/**
+ * VoyageRpcService defines all handlers for eth RPC methods.
+ * Used to implement the backing JsonRpcEngine of VoyageProvider.
+ */
+class VoyageRpcService {
+  store: ControllerStore;
+  constructor(store: ControllerStore) {
+    this.store = store;
+  }
+
+  handleEthAccounts = async () => {
+    const { address } = await this.store.keyStore.getAccount();
+    return [address];
+  };
+}
+
+export default VoyageRpcService;
