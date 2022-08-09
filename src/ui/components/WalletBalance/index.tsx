56e5ada6f602991bafc53efce00e8fe38eaff2ff
daulet <daulet.issatayev@nu.edu.kz>
2022-08-09 13:00:20 +0600

    feat: await state when deposited enough

---
+++ src/ui/components/WalletBalance/index.tsx
@@ -0,0 +1,51 @@
+import Text from '@components/Text';
+import { Group, GroupProps } from '@mantine/core';
+import BigNumber from 'bignumber.js';
+import * as React from 'react';
+import { useInterval } from '@mantine/hooks';
+import { formatEthersBN, Zero } from '@utils/bn';
+import { ReactComponent as EthSvg } from 'assets/img/eth-icon.svg';
+import { ReactComponent as DangerSvg } from 'assets/img/danger-icon.svg';
+import { ReactComponent as CheckSvg } from 'assets/img/circle-check-icon.svg';
+
+type IWalletBalanceProps = GroupProps & {
+  address: string;
+  minBalance: BigNumber;
+  onEnoughBalance: () => void;
+};
+
+const WalletBalance: React.FunctionComponent<IWalletBalanceProps> = ({
+  minBalance,
+  address,
+  onEnoughBalance,
+}) => {
+  const [balance, setBalance] = React.useState(Zero);
+
+  const pollBalance = React.useCallback(async () => {
+    const res = formatEthersBN(await controller.getBalance(address));
+    setBalance(res);
+    if (minBalance.lte(res)) onEnoughBalance();
+  }, [address]);
+
+  const balancePoll = useInterval(pollBalance, 5000);
+
+  React.useEffect(() => {
+    pollBalance();
+    balancePoll.start();
+    return balancePoll.stop;
+  }, []);
+  return (
+    <Group direction="column" align="center" spacing={0}>
+      <Text>Your Voyage Wallet Balance</Text>
+      <Group mt={5} align="center" spacing={2} ml={32}>
+        <Text sx={{ fontSize: 24 }} weight={'bold'}>
+          {balance.toString()}
+        </Text>
+        <EthSvg />
+        {minBalance.lte(balance) ? <CheckSvg /> : <DangerSvg />}
+      </Group>
+    </Group>
+  );
+};
+
+export default WalletBalance;
