dc89ddec508441254887e607b4e463f42598f9f8
daulet <daulet.issatayev@nu.edu.kz>
2022-09-21 15:57:22 +0600

    feat: bottom navigation bar

---
+++ src/ui/components/NavigationBar/index.tsx
@@ -0,0 +1,59 @@
+import { Box, Group } from '@mantine/core';
+import * as React from 'react';
+import DiscoverIconSrc from '@images/navigation/menu-discover.svg';
+import PaperIconSrc from '@images/navigation/menu-paper.svg';
+import SteeringIconSrc from '@images/navigation/menu-steering.svg';
+import SwordIconSrc from '@images/navigation/menu-sword.svg';
+import styles from './index.module.scss';
+import cn from 'classnames';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+type NavigationItem = {
+  path: string;
+  icon: string;
+};
+
+const items: NavigationItem[] = [
+  {
+    path: '/',
+    icon: DiscoverIconSrc,
+  },
+  {
+    path: '/sword',
+    icon: SwordIconSrc,
+  },
+  {
+    path: '/bills',
+    icon: PaperIconSrc,
+  },
+  {
+    path: '/management',
+    icon: SteeringIconSrc,
+  },
+];
+
+const NavigationBar: React.FunctionComponent = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <Group px={30} mt="auto" className={styles.navigation} spacing={0}>
+      {items.map((item, index) => {
+        return (
+          <Box
+            className={cn(
+              styles.navigationItem,
+              location.pathname == item.path && styles.active
+            )}
+            key={index}
+            onClick={() => navigate(item.path)}
+          >
+            <img src={item.icon} className={styles.icon} />
+            <div className={styles.border} />
+          </Box>
+        );
+      })}
+    </Group>
+  );
+};
+
+export default NavigationBar;
