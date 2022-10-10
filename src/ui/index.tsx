b572c2835800fed5a88f39656c06cb8fca70c1e5
iantanwx <ian.tan@voyage.finance>
2022-10-10 14:16:17 +0800

    feat: add sentry

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import { initWeb3 } from '@web3/init';
 import { initStore } from '@state/store';
 import App from './App';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.2 : 1.0,
+});
 
 async function bootstrap() {
   // set up a connection to background script
