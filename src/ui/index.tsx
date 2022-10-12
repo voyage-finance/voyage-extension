8c6de486f18f1740eefd7d28f57685bb8381bf37
daulet <daulet.issatayev@nu.edu.kz>
2022-10-12 22:45:18 +0600

    feat: reset.html to retry login flow

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -5,6 +5,7 @@ import { BrowserTracing } from '@sentry/tracing';
 import { initWeb3 } from '@web3/init';
 import { initStore } from '@state/store';
 import App from './App';
+import { ExtensionEnvType, getEnvironmentType } from '@utils/extension';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -16,6 +17,11 @@ async function bootstrap() {
   // set up a connection to background script
   initWeb3();
   const store = await initStore();
+  const isResetFlow = getEnvironmentType() === ExtensionEnvType.Reset;
+  if (isResetFlow) {
+    controller.cancelLogin();
+    window.open('/home.html', '_self');
+  }
   ReactDOM.render(
     <React.StrictMode>
       <App store={store} />
