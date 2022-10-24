94907c8f0a090ea0734ff4d01fe0abb9e55f20a2
daulet <daulet.issatayev@nu.edu.kz>
2022-10-24 15:29:06 +0600

    feat: asset sent succes page

---
+++ src/ui/pages/send/SuccessForm/index.tsx
@@ -0,0 +1,136 @@
+import { TOKEN } from '@components/moleculas/CurrencySelector';
+import { Group, Image, Stack, TextInput } from '@mantine/core';
+import { ReactComponent as WethSvg } from 'assets/img/weth.svg';
+import { ReactComponent as EthSvg } from 'assets/img/eth-icon.svg';
+import * as React from 'react';
+import Text from '@components/Text';
+import PepePlacholderImg from '@images/pepe-placeholder.png';
+import { ArrowUpRight } from 'tabler-icons-react';
+import { CollectionAsset } from 'types';
+import { SentAssetType, TransferType } from '..';
+import { getTxExplorerLink } from '@utils/env';
+import Button from '@components/Button';
+import styles from './index.module.scss';
+
+const NftSuccessForm: React.FC<{
+  nft: CollectionAsset;
+  recipient: string;
+}> = ({ nft, recipient }) => {
+  return (
+    <Stack pb={60}>
+      <Group
+        py={6}
+        px={8}
+        spacing={0}
+        sx={{
+          borderRadius: 10,
+          background: 'rgba(27, 29, 44, 0.6)',
+          ':hover': { cursor: 'pointer' },
+        }}
+        align="center"
+      >
+        <Image
+          width={38}
+          height={38}
+          fit="contain"
+          radius={10}
+          src={nft?.image || PepePlacholderImg}
+          alt="image"
+        />
+        <Stack spacing={0} ml={8}>
+          <Text weight="bold">{nft.name || `#${nft.tokenId}`}</Text>
+          <Text type="secondary" size="sm">
+            {nft.collection.name}
+          </Text>
+        </Stack>
+      </Group>
+      <TextInput
+        size="md"
+        value={recipient}
+        className={styles.input}
+        readOnly
+      />
+    </Stack>
+  );
+};
+
+const TokenSuccessForm: React.FC<{
+  token: TOKEN;
+  recipient: string;
+  amount: string;
+}> = ({ token, recipient, amount }) => {
+  return (
+    <Stack pb={60}>
+      <Group
+        py={6}
+        px={8}
+        spacing={0}
+        sx={{
+          borderRadius: 10,
+          background: 'rgba(27, 29, 44, 0.6)',
+          ':hover': { cursor: 'pointer' },
+        }}
+        align="center"
+      >
+        {token == TOKEN.ETH ? <EthSvg /> : <WethSvg />}
+        <Stack spacing={0} ml={8}>
+          <Text weight="bold">
+            {token == TOKEN.ETH ? 'Ethereum' : 'Wrapped Ethereum'}
+          </Text>
+        </Stack>
+      </Group>
+      <TextInput
+        className={styles.input}
+        size="md"
+        value={recipient}
+        readOnly
+      />
+      <TextInput
+        className={styles.input}
+        type="number"
+        rightSection={
+          <Text type="gradient" weight="bold">
+            {token == TOKEN.WETH ? 'wETH' : 'ETH'}
+          </Text>
+        }
+        readOnly
+        size="md"
+        value={amount}
+      />
+    </Stack>
+  );
+};
+
+const SuccessForm: React.FunctionComponent<{
+  asset: SentAssetType;
+  onClose: () => void;
+}> = ({ asset, onClose }) => {
+  return (
+    <Stack pb={60}>
+      {asset.type == TransferType.TOKEN ? (
+        <TokenSuccessForm
+          token={asset.value as TOKEN}
+          recipient={asset.recipient}
+          amount={asset.amount!}
+        />
+      ) : (
+        <NftSuccessForm
+          nft={asset.value as CollectionAsset}
+          recipient={asset.recipient}
+        />
+      )}
+      <Button
+        fullWidth
+        mt={12}
+        onClick={() => window.open(getTxExplorerLink(asset.txHash), '_blank')}
+      >
+        View Send Txn <ArrowUpRight size={16} />
+      </Button>
+      <Button fullWidth mt={12} onClick={onClose}>
+        Close
+      </Button>
+    </Stack>
+  );
+};
+
+export default SuccessForm;
--- src/ui/pages/send/index.tsx
+++ src/ui/pages/send/index.tsx
@@ -1,49 +1,104 @@
 import TitleWithLine from '@components/atoms/TitleWithLine';
+import { TOKEN } from '@components/moleculas/CurrencySelector';
 import { SegmentedControl, Stack } from '@mantine/core';
 import * as React from 'react';
+import { CollectionAsset } from 'types';
 import styles from './index.module.scss';
 import NFTForm from './NFTForm';
+import SuccessForm from './SuccessForm';
 import TokenForm from './TokenForm';
 
-enum TransferType {
+export enum TransferType {
   TOKEN = 'Token',
   NFT = 'NFT',
 }
 
+export type SentAssetType = {
+  type: TransferType;
+  value: TOKEN | CollectionAsset;
+  recipient: string;
+  amount?: string;
+  txHash: string;
+};
+
 const SendPage: React.FunctionComponent = () => {
-  const [currentTab, setCurrentTab] = React.useState(TransferType.TOKEN);
+  const [currentTab, setCurrentTab] = React.useState<TransferType>(
+    TransferType.TOKEN
+  );
+  const [sentAsset, setSentAsset] = React.useState<SentAssetType>();
+
   return (
     <div className={styles.wrapper}>
       <Stack className={styles.root}>
-        <TitleWithLine showClose>Send</TitleWithLine>
-        <SegmentedControl
-          data={[
-            { label: TransferType.TOKEN, value: TransferType.TOKEN },
-            { label: TransferType.NFT, value: TransferType.NFT },
-          ]}
-          color="black"
-          value={currentTab}
-          onChange={(value) => setCurrentTab(value as TransferType)}
-          styles={{
-            root: {
-              backgroundColor: 'rgba(27, 29, 44, 0.6)',
-              borderRadius: 10,
-              padding: '5px, 6px',
-              flexShrink: 0,
-            },
-            label: {
-              fontWeight: 700,
-              fontSize: 14,
-              color: 'white',
-            },
-            labelActive: {
-              backgroundColor: 'rgba(255, 255, 255, 0.1)',
-              borderRadius: 5,
-            },
-          }}
-        />
-        {currentTab == TransferType.NFT && <NFTForm />}
-        {currentTab == TransferType.TOKEN && <TokenForm />}
+        <TitleWithLine
+          showClose
+          onClose={
+            sentAsset
+              ? () => {
+                  setSentAsset(undefined);
+                }
+              : undefined
+          }
+        >
+          Send
+        </TitleWithLine>
+        {!sentAsset && (
+          <SegmentedControl
+            data={[
+              { label: TransferType.TOKEN, value: TransferType.TOKEN },
+              { label: TransferType.NFT, value: TransferType.NFT },
+            ]}
+            color="black"
+            value={currentTab}
+            onChange={(value) => setCurrentTab(value as TransferType)}
+            styles={{
+              root: {
+                backgroundColor: 'rgba(27, 29, 44, 0.6)',
+                borderRadius: 10,
+                padding: '5px, 6px',
+                flexShrink: 0,
+              },
+              label: {
+                fontWeight: 700,
+                fontSize: 14,
+                color: 'white',
+              },
+              labelActive: {
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                borderRadius: 5,
+              },
+            }}
+          />
+        )}
+        {sentAsset ? (
+          <SuccessForm
+            asset={sentAsset}
+            onClose={() => setSentAsset(undefined)}
+          />
+        ) : currentTab == TransferType.NFT ? (
+          <NFTForm
+            onSent={(value, recipient, txHash) =>
+              setSentAsset({
+                type: TransferType.NFT,
+                value,
+                recipient,
+                txHash,
+              })
+            }
+          />
+        ) : (
+          <TokenForm
+            onSent={(value, recipient, amount, txHash) =>
+              setSentAsset({
+                type: TransferType.TOKEN,
+                value,
+                recipient,
+                amount,
+                txHash,
+              })
+            }
+          />
+        )}
       </Stack>
     </div>
   );
